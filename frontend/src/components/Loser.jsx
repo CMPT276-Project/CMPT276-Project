c5fb70f63b523d384053c943ec6eac6cc958c2fc
Jaewon <jaewon.h4@gmail.com>
2023-11-26 21:29:37 -0800

    Added a api call to the Giphy API, implemented all functions to extract api data and display on screen

--- frontend/src/components/Loser.jsx
+++ frontend/src/components/Loser.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "../styles/Loser.css"
 import {useNavigate} from 'react-router-dom'
-
+import axios from 'axios'
 function Loser({ dataFromParent }) {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  const [gif, setGif] = useState("")
+  const [renderedImage, setRenderedImage] = useState(null)
+
+  const limit = 5
+  const apiKey = "1NscCn7Jf3oBH8kb4Ew4BicIpRbY6KD0"
+  const apiURL = "https://api.giphy.com/v1/gifs/search"
+  const params = {
+    'api_key': apiKey,
+    'q': 'lose',
+    'rating': 'pg-13',
+    'limit': 50
+  }
+
+  const shuffleArray = (array) => {
+    const shuffledArray = array.sort(() => Math.random() - 0.5)
+    return shuffledArray
+  }
+
+  async function fetchGIF() {
+    try {
+      const response = await axios.get(apiURL, {params})
+      const responseData = response.data.data // array
+      const newResponseData = shuffleArray(responseData) // randomize responseData array
+      setGif(newResponseData[1].images.original.url)
+    } catch (error) {
+      console.error("Error fetching response data: ", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchGIF()
+  }, [dataFromParent])
+
+  useEffect(() => {
+    setRenderedImage(
+      <img className="loser-image" src={gif} />
+    )
+  }, [gif])
+
   return (
     <div className="loser-page">
         <p className="status">You Lose</p>
+        {renderedImage}
         <div className="score-board">Your Score: {dataFromParent} </div>
         <div className="play-again" onClick={() => navigate('/')}>Play Again</div>
     </div>
--- frontend/src/components/Winner.jsx
+++ frontend/src/components/Winner.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "../styles/Winner.css"
 import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 function Winner({ dataFromParent }) {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  const [gif, setGif] = useState("")
+  const [renderedImage, setRenderedImage] = useState(null)
+
+  const apiKey = "1NscCn7Jf3oBH8kb4Ew4BicIpRbY6KD0"
+  const apiURL = "https://api.giphy.com/v1/gifs/search"
+  const params = {
+    'api_key': apiKey,
+    'q': 'win',
+    'rating': 'pg-13',
+    'limit': 50
+  }
+
+  const shuffleArray = (array) => {
+    const shuffledArray = array.sort(() => Math.random() - 0.5)
+    return shuffledArray
+  }
+
+  async function fetchGIF() {
+    try {
+      const response = await axios.get(apiURL, {params})
+      const responseData = response.data.data // array
+      const newResponseData = shuffleArray(responseData) // randomize responseData array
+      setGif(newResponseData[1].images.original.url)
+    } catch (error) {
+      console.error("Error fetching response data: ", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchGIF()
+  }, [dataFromParent])
+
+  useEffect(() => {
+    setRenderedImage(
+      <img className="winner-image" src={gif} />
+    )
+  }, [gif])
+
   return (
     <div className="winner-page">
         <p className="status">You Win</p>
+        {renderedImage}
         <div className="score-board">Your Score: {dataFromParent} </div>
         <div className="play-again" onClick={() => navigate('/') }>Play Again</div>
     </div>
