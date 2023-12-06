68c7b10dbe8713db7098821bd8ff76ad27019354
Kelvin Hu <kha115@sfu.ca>
2023-12-06 07:41:39 -0800

    Granularized testing suite

--- backend/tests/database.test.js
+++ backend/tests/database.test.js
@@ -26,22 +26,26 @@ describe("Users", function() {
         );
     });
 
-    test("Update username", async function() {
-        await update_user_name(db, guid, name,
-            function() { },
-            function(error) {
-                // TODO: Unsure what to test for this branch.
-            }
-        );
+    describe("Updating username", function () {
+        test("Inserting into database", async function() {
+            await update_user_name(db, guid, name,
+                function() { },
+                function(error) {
+                    // TODO: Unsure what to test for this branch.
+                }
+            );
+        });
 
-        await get_user(db, guid,
-            function(record) {
-                expect(record.name).toBe(name);
-            },
-            function(error) {
-                // TODO: Unsure what to test for this branch.
-            }
-        );
+        test("Checking if successfully inserted", async function() {
+            await get_user(db, guid,
+                function(record) {
+                    expect(record.name).toBe(name);
+                },
+                function(error) {
+                    // TODO: Unsure what to test for this branch.
+                }
+            );
+        });
     });
 });
 
@@ -72,57 +76,70 @@ describe("Scores", function() {
         );
     });
 
-    test("Update user high score", async function() {
-        await update_user_score_by_amount(db, guid, score_current,
-            function() { },
-            function(error) {
-                // TODO: Unknown test case
-            }
-        );
 
-        await get_score_for_user(db, guid, 
-            function(score) {
-                expect(score).toBe(score_current);
-            },
-            function(error) {
-                // TODO: Unsure what to test for this branch
-            }
-        );
+    describe("Updating user high score from 0", function() {
+        test("Update score from 0", async function() {
+            await update_user_score_by_amount(db, guid, score_current,
+                function() { },
+                function(error) {
+                    // TODO: Unknown test case
+                }
+            );
+        });
+
+        test("Check if score was updated from 0", async function() {
+            await get_score_for_user(db, guid, 
+                function(score) {
+                    expect(score).toBe(score_current);
+                },
+                function(error) {
+                    // TODO: Unsure what to test for this branch
+                }
+            );
+        });
+    });
+
+    describe("Update user high score - Score is less than stored", function() {
+        test("Update with lower score than stored", async function() {
+            await update_user_score_by_amount(db, guid, score_less,
+                function() { },
+                function(error) {
+                    // TODO: Unsure what to test for this branch.
+                }
+            );
+        });
+
+        test("Check if score was NOT updated", async function() {
+            await get_score_for_user(db, guid, 
+                function(score) {
+                    expect(score).toBe(score_current);
+                },
+                function(error) {
+                    // TODO: Unsure what to test for this branch
+                }
+            );
+        })
     });
     
-    test("Update user high score - Score is less than stored", async function() {
-        await update_user_score_by_amount(db, guid, score_less,
-            function() { },
-            function(error) {
-                // TODO: Unsure what to test for this branch.
-            }
-        );
+    describe("Update user high score - Score is greater than stored", function() {
+        test("Update with greater score than stored", async function() {
+            await update_user_score_by_amount(db, guid, score_more,
+                function() { },
+                function(error) {
+                    // TODO: Unsure what to test for this branch.
+                }
+            );
+        });
 
-        await get_score_for_user(db, guid, 
-            function(score) {
-                expect(score).toBe(score_current);
-            },
-            function(error) {
-                // TODO: Unsure what to test for this branch
-            }
-        );
+        test("Check if score was updated.", async function() {
+            await get_score_for_user(db, guid, 
+                function(score) {
+                    expect(score).toBe(score_more);
+                },
+                function(error) {
+                    // TODO: Unsure what to test for this branch
+                }
+            );
+        });
     });
-
-    test("Update user high score - Score is higher than stored", async function() {
-        await update_user_score_by_amount(db, guid, score_more,
-            function() { },
-            function(error) {
-                // TODO: Unsure what to test for this branch.
-            }
-        );
-
-        await get_score_for_user(db, guid, 
-            function(score) {
-                expect(score).toBe(score_more);
-            },
-            function(error) {
-                // TODO: Unsure what to test for this branch
-            }
-        );
-    });
-})
+});
